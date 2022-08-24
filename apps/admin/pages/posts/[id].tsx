8f74ea61255087bb2ebea434c1ff6f69a7104a16
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2022-08-24 11:28:12 -0500

    feat: Updated old code

--- apps/admin/pages/posts/[id].tsx
+++ apps/admin/pages/posts/[id].tsx
@@ -1,8 +1,8 @@
-import Layout from '../../components/layout'
+// import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-import Head from 'next/head'
-import Date from '../../components/date'
-import utilStyles from '../../styles/utils.module.css'
+// import Head from 'next/head'
+// import Date from '../../components/date'
+// import utilStyles from '../../styles/utils.module.css'
 import { GetStaticProps, GetStaticPaths } from 'next'
 
 export default function Post({
@@ -15,18 +15,19 @@ export default function Post({
   }
 }) {
   return (
-    <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
-    </Layout>
+    <h1>Post</h1>
+    // <Layout>
+    //   <Head>
+    //     <title>{postData.title}</title>
+    //   </Head>
+    //   <article>
+    //     <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+    //     <div className={utilStyles.lightText}>
+    //       <Date dateString={postData.date} />
+    //     </div>
+    //     <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    //   </article>
+    // </Layout>
   )
 }
 
