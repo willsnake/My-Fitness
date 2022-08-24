c273c3193d63f3b83bc375e9c988ce8552d077d7
Daniel Rodriguez Monroy <willsnake87@gmail.com>
2022-08-24 11:27:38 -0500

    feat(apps/admin/pages/_app.tsx): Added new layout loading

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/global.css'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import "styles/tailwind.css"
+
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return getLayout(<Component {...pageProps} />)
 }
